dcfbc0a99ff88d0e53a89b4ffd2798e9a16085e8
Lucas Eduardo <lucas.eduardo@m4u.com.br>
2022-02-18 00:20:13 -0300

    feature/Create connection with database

---
+++ pkg/database/db-config.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+func CreateDbConnection() *mongo.Collection {
+
+	var ctx = context.TODO()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("tweets").Collection("twwet")
+
+	return collection
+}
